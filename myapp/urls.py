a348da080dea79ee587778aec3d0dfa77a4ce35c
Colten Bayles <cbayles@outlook.com>
2023-11-27 20:35:09 -0500

    V 0.0.1.2 - Added conversion logic to change btc too usd along with front end to show convertion

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import convert_usd_to_btc
 
 urlpatterns = [
     path('', views.main, name='main'),
     path('bitcoin-price/', views.bitcoin_price_view, name='bitcoin_price'),
     path('live_metrics/', views.live_metrics, name='live_metrics'),
+    path('convert-usd-to-btc/', convert_usd_to_btc, name='convert_usd_to_btc'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .utils import get_bitcoin_price
+import requests
 from django.http import JsonResponse
 from .forms import ExpenseForm
 from .models import Expense
@@ -25,3 +26,16 @@ def index(request):
         form = ExpenseForm()
     expenses = Expense.objects.all()
     return render(request, 'main.html', {'form': form, 'expenses': expenses})
+
+def get_bitcoin_to_usd_rate():
+    url = "https://bitpay.com/api/rates"
+    response = requests.get(url)
+    data = response.json()
+    return data[2]["rate"]  # Index might change based on API response
+
+def convert_usd_to_btc(request):
+    usd_price = request.GET.get('usd_price', 10)  # Default to 10 if no input
+    usd_price = float(usd_price)
+    rate = get_bitcoin_to_usd_rate()
+    bitcoin_price = round(usd_price / rate, 8)
+    return JsonResponse({'usd_price': usd_price, 'bitcoin_price': bitcoin_price})
