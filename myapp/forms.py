34cbf810c9a4f634952fef40c81b3962618400b8
Colten Bayles <cbayles@outlook.com>
2023-12-12 19:19:47 -0500

    V 0.2.3.1 - Working On fixing forms

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,11 +1,7 @@
 from django import forms
 from .models import Expense
-from django.forms.widgets import DateInput
 
 class ExpenseForm(forms.ModelForm):
     class Meta:
         model = Expense
         fields = ['up_front_cost', 'daily_cost', 'daily_income', 'installation_date']
-        widgets = {
-             'installation_date': DateInput(attrs={'type': 'date'}),
-        }
