32f48cee0c030cd81ca8ef5879bb9a3ebe05255b
Colten Bayles <cbayles@outlook.com>
2023-12-12 19:11:14 -0500

    V 0.2.3 - Fixed Forms.py and added functionality. Attached form to main.html

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from .models import Expense
+from django.forms.widgets import DateInput
 
 class ExpenseForm(forms.ModelForm):
     class Meta:
         model = Expense
         fields = ['up_front_cost', 'daily_cost', 'daily_income', 'installation_date']
+        widgets = {
+             'installation_date': DateInput(attrs={'type': 'date'}),
+        }
